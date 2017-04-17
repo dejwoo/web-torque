59c3871db0c0c05f934fefb16eb5433440526458
Koszeghy David <david.koszeghy@softec.sk>
2017-04-17 22:01:56 +0200

    develop: nav+bootstrap4

---
+++ client/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { AuthenticationService } from '../_services/index';
+
+@Component({
+    moduleId: module.id.toString(),
+    templateUrl: 'nav.component.html',
+    selector: 'app-nav',
+})
+
+export class NavComponent implements OnInit {
+    currentUser: User;
+    constructor(authenticationService:AuthenticationService) {
+        authenticationService.isLoggedIn().subscribe(user => this.currentUser = user);
+    }
+    ngOnInit() {}
+}
